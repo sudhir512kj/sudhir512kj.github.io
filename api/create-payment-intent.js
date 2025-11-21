7ca50b5ba641ebfc35ba025224e3f393cac36177
sudhir meena <sureshmeena512@gmail.com>
2025-11-21 14:00:15 +0530

    payment gateway fixes

---
+++ api/create-payment-intent.js
@@ -0,0 +1,21 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const { amount, planId } = req.body;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: 'inr',
+      metadata: { planId },
+    });
+
+    res.status(200).json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
